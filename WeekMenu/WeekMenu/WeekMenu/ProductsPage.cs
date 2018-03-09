60d73bdc988d4c3c41c139472b02dd7e0ee245d8
Serge Listishenkov <SerSX@icloud.com>
2018-03-09 12:58:31 +0500

    Create ProductsPage

---
+++ WeekMenu/WeekMenu/WeekMenu/ProductsPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace WeekMenu
+{
+    public class ProductsPage : ContentPage
+    {
+        public ProductsPage()
+        {
+            Title = "Продукты";
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    new Label
+                    {
+                        Text = "Морковь 1.5 кг"
+                    },
+                    new Label
+                    {
+                        Text = "Огурец 1кг"
+                    },
+                    new Label
+                    {
+                        Text = "Картофель 3кг"
+                    },
+                    new Label
+                    {
+                        Text = "Лук 1кг"
+                    },
+                    new Label
+                    {
+                        Text = "Федук 73кг"
+                    },
+                }
+            };
+        }
+
+    }
+}
